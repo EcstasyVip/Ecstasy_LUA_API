a38fa3903408d9948e5a3f44fe0d9d9a23c449d9
Felton <98848776+xtcfelton@users.noreply.github.com>
2022-02-03 21:03:51 +0100

    Update drawFPS.lua

--- examples/drawFPS.lua
+++ examples/drawFPS.lua
@@ -1,11 +1,11 @@
 --draw FPS
 
 local function drawfps()
-    render.text(100, 100, color.new(220, 11, 220, 255), tostring(math.floor(1/ globals.get_frametime())))
-  --[[
-  globals.get_frametime() is the amount of ms it takes to render a frame
-  math.floor gets rid of the fraction for us and makes a nice looking integer
-  ]]--
+    render.text(100, 100, color.new(220, 11, 220, 255), tostring(math.floor(1 / globals.get_frametime())))
+    --[[
+        globals.get_frametime() is the amount of ms it takes to render a frame
+        math.floor gets rid of the fraction for us and makes a nice looking integer
+    ]]--
 end
 
 cheat.register_callback("on_paint", drawfps)
