9313c28c3548c1293ae797b6abd0f4652be7de14
XTCHOSS <98845361+EcstasyVip@users.noreply.github.com>
2022-07-30 21:48:45 +0200

    Update drawFPS.lua

--- examples/drawFPS.lua
+++ examples/drawFPS.lua
@@ -1,7 +1,7 @@
 --draw FPS
 
 local function drawfps()
-    render.text(100, 100, color.new(220, 11, 220, 255), tostring(math.floor(1 / globals.get_frametime())))
+    render.text(100, 100, color.new(220, 11, 220, 255), 0, "mainfont", tostring(math.floor(1 / globals.get_frametime())))
     --[[
         globals.get_frametime() is the amount of ms it takes to render a frame
         math.floor gets rid of the fraction for us and makes a nice looking integer
